17634e14312742333db3876cdb39015dcd4867f1
kartavya <kartavyas@gmail.com>
2024-01-18 14:09:41 +1100

    product list page added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ProductList from "./features/productList/ProductList";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList></ProductList>
+    </div>
+  );
+}
+
+export default App;
