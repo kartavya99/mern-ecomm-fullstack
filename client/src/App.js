64180f5f700c63afc19dc34d405c43c24fbed99e
kartavya <kartavyas@gmail.com>
2024-01-18 18:13:56 +1100

    Login & Singup page added with react-router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Singup from "../features/auth/components/Singup";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Singup></Singup>
+    </div>
+  );
+};
+
+export default SignupPage;
