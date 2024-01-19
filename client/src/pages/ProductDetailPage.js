dcd29c68fc1a6edcaf82ad5962d116da0735057a
kartavya <kartavyas@gmail.com>
2024-01-19 11:27:22 +1100

    product list imported dynamically from json server api

--- client/src/pages/ProductDetailPage.js
+++ client/src/pages/ProductDetailPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../features/navbar/Navbar";
-import ProductDetails from "../features/productList/components/ProductDetails";
+import ProductDetails from "../features/product/components/ProductDetails";
 
 const ProductDetailPage = () => {
   return (
